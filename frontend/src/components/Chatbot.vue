535564fd547e596dc9c838614460bd87e35f14ed
imxuan03 <txuan0164@gmail.com>
2024-09-05 10:57:45 +0700

    push frontend

--- frontend/src/components/Chatbot.vue
+++ frontend/src/components/Chatbot.vue
@@ -1,17 +1,24 @@
 <template>
-    <div class="chatbot">
-        <div class="chat-window">
-            <div v-for="(message, index) in messages" :key="index" class="message">
-                <p :class="{ 'user-message': message.from === 'user', 'bot-message': message.from === 'bot' }">
-                    {{ message.text }}
-                </p>
+    <div class="chatbot-container">
+        <button class="toggle-button" @click="toggleChatbot">
+            <div v-if="!isOpen" id="icon-open"><i class="fa-regular fa-message"></i></div>
+            <div v-else id="icon-close"><i class="fa-solid fa-minus"></i></div>
+        </button>
+        <div v-if="isOpen" class="chatbot">
+            <div class="chat-window">
+                <div v-for="(message, index) in messages" :key="index" class="message">
+                    <p :class="{ 'user-message': message.from === 'user', 'bot-message': message.from === 'bot' }">
+                        {{ message.text }}
+                    </p>
+                </div>
             </div>
-        </div>
-        <div class="input-area">
-            <input v-model="userInput" @keyup.enter="sendMessage" placeholder="Type your message here..." />
+            <div class="input-area">
+                <input v-model="userInput" @keyup.enter="sendMessage" placeholder="Type your message here..." />
+            </div>
         </div>
     </div>
 </template>
+
 
 <script>
 import ChatbotService from "../services/chatbot.service";
@@ -19,11 +26,15 @@ import ChatbotService from "../services/chatbot.service";
 export default {
     data() {
         return {
+            isOpen: false,
             messages: [],
             userInput: '',
         };
     },
     methods: {
+        toggleChatbot() {
+            this.isOpen = !this.isOpen;
+        },
         async sendMessage() {
             if (this.userInput.trim()) {
                 this.messages.push({ text: this.userInput, from: 'user' });
@@ -45,14 +56,36 @@ export default {
 </script>
 
 <style scoped>
+
+.chatbot-container {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+}
+
+#icon-open{
+    font-size: 25px;
+    padding: 10px;
+}
+
+.toggle-button{
+    padding: 5px 10px 5px 10px;
+    border: none;
+    background-color: #007bff;
+    color: white;
+    cursor: pointer;
+    border-radius: 50%;
+    z-index: 100;
+}
+
 .chatbot {
-    width: 300px;
+    width: 350px;
     border: 1px solid #ccc;
     border-radius: 5px;
     overflow: hidden;
     display: flex;
     flex-direction: column;
-    height: 400px;
+    height: 450px;
 }
 
 .chat-window {
